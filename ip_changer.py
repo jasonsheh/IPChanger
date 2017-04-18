0fa50de49134b942fcff4518ceb7aa3e71b4ed8d
jasonsheh <qq3039344@gmail.com>
2017-04-18 22:51:21 +0800

    doesn't work

--- ip_changer.py
+++ ip_changer.py
@@ -4,63 +4,75 @@
 
 import winreg
 import binascii
-
-key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
-                     0, winreg.KEY_ALL_ACCESS)
-
-# 获取该键的所有键值，遍历枚举
+import ctypes
 
 
 def to_byte(obj):
     return str(obj).encode(encoding="utf-8")
 
-try:
-    # value, _type = winreg.QueryValueEx(key, "DefaultConnectionSettings")
 
-    ip = b'110.77.197.111:8080'
-    ip_length = to_byte(hex(len(ip)).replace('0x', ''))
+def pppoe_changer():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                             r'Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections',
+                             0, winreg.KEY_ALL_ACCESS)
 
-    ip_length_hex = binascii.hexlify(ip_length)
-    ip_hex = binascii.hexlify(ip)
+        ip = b'ip'
+        ip_length_hex = to_byte(hex(len(ip)).replace('0x', ''))
+        ip_hex = binascii.hexlify(ip)
 
-    # print(ip_length)
-    # print(ip_length_hex)
-    # print(ip_hex)
+        value = b'46000000da020000' \
+                b'03000000%s000000' \
+                b'%s00000000210000' \
+                b'0100000000000000' \
+                b'0000000000000000' \
+                b'0000000000000000' \
+                b'0000000000000000' % (ip_length_hex, ip_hex)
 
-    value = b'46000000da020000' \
-            b'03000000%s000000' \
-            b'%s00000000210000' \
-            b'0100000000000000' \
-            b'0000000000000000' \
-            b'0000000000000000' \
-            b'0000000000000000' % (ip_length, ip_hex)
+        default_value = b'46000000da020000' \
+                        b'0100000000000000' \
+                        b'0000000000000000' \
+                        b'0100000000000000' \
+                        b'0000000000000000' \
+                        b'0000000000000000' \
+                        b'0000000000000000'
+        # enable
+        winreg.SetValueEx(key, "DefaultConnectionSettings", 0, winreg.REG_BINARY, binascii.unhexlify(value))
 
-    default_value = b'46000000da020000' \
-                    b'0100000000000000' \
-                    b'0000000000000000' \
-                    b'0100000000000000' \
-                    b'0000000000000000' \
-                    b'0000000000000000' \
-                    b'0000000000000000'
+        # disable
+        winreg.SetValueEx(key, "DefaultConnectionSettings", 0, winreg.REG_BINARY, binascii.unhexlify(default_value))
 
-    '''
-    value = r'4600000da020000' \
-            r'01000000%s000000' \
-            r'0000000000000000' \
-            r'0100000000000000' \
-            r'0000000000000000' \
-            r'0000000000000000' \
-            r'0000000000000000' % (to_hex(ip_length))
-    '''
+        key.Close()
+    except WindowsError:
+        print('error')
 
-    winreg.SetValueEx(key, "DefaultConnectionSettings", 0, winreg.REG_BINARY, binascii.unhexlify(value))
-    # print(value, '\n', _type)
-    # print(value)
-    # print(binascii.unhexlify(value))
-    # print(ip_hex)
 
-    key.Close()
+def lan_changer():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                                 r'Software\Microsoft\Windows\CurrentVersion\Internet Settings',
+                                 0, winreg.KEY_ALL_ACCESS)
+        flag = int(input('flag:'))
 
-except WindowsError:
-    print('error')
+        if flag:
+            # enable
+            print('启用代理')
+            winreg.SetValueEx(key, "ProxyEnable", 0, winreg.REG_DWORD, 1)
+            winreg.SetValueEx(key, "ProxyServer", 0, winreg.REG_SZ, "202.121.96.33:8086")
+        else:
+            # disable
+            print('取消代理')
+            winreg.SetValueEx(key, "ProxyEnable", 0, winreg.REG_DWORD, 0)
+    except WindowsError:
+        print('error')
 
+    internet_set_option = ctypes.windll.Wininet.InternetSetOptionW
+    internet_set_option(0, 37, 0, 0)
+    internet_set_option(0, 39, 0, 0)
+
+
+def main():
+    lan_changer()
+
+if __name__ == '__main__':
+    main()
